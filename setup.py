71af8aef863ee321a66457d003d011fa35086074
Chris Kloberdanz <klobyone@gmail.com>
2013-03-13 23:39:43 -0500

    Added method descriptions. Updated setup.py and README for dist.

--- setup.py
+++ setup.py
@@ -1,16 +1,22 @@
 from distutils.core import setup, Extension
 
-module1 = Extension('PyBCM2835',
+pybcm2835 = Extension('PyBCM2835',
 					include_dirs = ['/usr/local/include'],
 					libraries = ['bcm2835'],
 					library_dirs = ['/usr/local/lib'],
                     sources = ['src/PyBCM2835.c'])
 
 setup (name = 'PyBCM2835',
-       version = '0.1',
-       description = 'Python extension for libcm2835.',
+       version = '0.1.0',
+       description = 'Python extension for libbcm2835.',
 	   author='Chris Kloberdanz',
 	   author_email='klobyone@gmail.com',
-	   license='GPL',
+	   license='GPLv2',
+       keywords=["raspberry pi", "bcm2835", "gpio"],
+	   classifiers=[
+	       "Programming Language :: Python",
+           "Development Status :: 3 - Alpha",
+	       "Topic :: System :: Hardware"
+	   ],
 	   url='http://www.github.com/klobyone/PyBCM2385/',
-	   ext_modules = [module1])
+	   ext_modules = [pybcm2835])
